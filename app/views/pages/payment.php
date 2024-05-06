93f6b7cbf6063cb2429a955b756e8f7f00f6e40c
Nimna-Silva <nimnasilva23@gmail.com>
2024-05-07 02:15:17 +0530

    edit nav

--- app/views/pages/payment.php
+++ app/views/pages/payment.php
@@ -13,26 +13,15 @@
       <img src="<?php echo URLROOT ?>/public/images/logo.png" alt="logo" width="120">
     </div>
     <div>
-      <a href="<?php echo URLROOT ?>/PageController/blog">Home</a>
+      <a class="active" href="#home">Home</a>
       <a href="<?php echo URLROOT ?>/PageController/blog">Blog</a>
       <a href="<?php echo URLROOT ?>/PageController/about">About Us</a>
       <a href="<?php echo URLROOT ?>/PageController/packages">Packages</a>
     </div>
-
-
-
-    <div class="profile-container">
-      <div class="profile">
-        <img src="<?php echo URLROOT ?>/public/images/u.png" alt="user" width="50">
-        <span class="username"></span>
-        <div class="dropdown-content">
-          <a href="#">Edit Profile</a>
-          <a href="#">Settings</a>
-          <a href="<?php echo URLROOT ?>/PageController/logout">Logout</a>
-        </div>
-      </div>
+    <div class="log">
+      <a href="<?php echo URLROOT ?>/PageController/signup">Signup</a>
+      <a href="<?php echo URLROOT ?>/PageController/signIn">Login</a>
     </div>
-  </div>
   </div>
 
 	<div class="padding">
