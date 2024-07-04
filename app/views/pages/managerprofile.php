a77486b3a146bc8e9d034fbb063936aa92c761bb
savindi <savindi.dasunika@gmail.com>
2024-07-04 17:56:59 +0530

    edit css

--- app/views/pages/managerprofile.php
+++ app/views/pages/managerprofile.php
@@ -1,137 +1,134 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-  <title> maneger profile</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin</title>
   <link rel="stylesheet" href="<?php echo URLROOT ?>/public/css/style_managerprofile.css">
 </head>
 
 <body>
-
-<div class="topnav">
+  <div class="topnav">
     <div class="test">
       <img src="<?php echo URLROOT ?>/public/images/logo.png" alt="logo" width="120">
     </div>
     <div>
-      <a class="active" href="<?php echo URLROOT ?>/PageController/home">home</a>
+      <a href="<?php echo URLROOT ?>/PageController/blog">Home</a>
       <a href="<?php echo URLROOT ?>/PageController/blog">Blog</a>
       <a href="<?php echo URLROOT ?>/PageController/about">About Us</a>
       <a href="<?php echo URLROOT ?>/PageController/packages">Packages</a>
     </div>
-    <div class="log">
-      <a href="<?php echo URLROOT ?>/PageController/signup">Signup</a>
-      <a href="<?php echo URLROOT ?>/PageController/signIn">Login</a>
+    <div class="profile-container">
+      <div class="profile">
+        <img src="<?php echo URLROOT ?>/public/images/u.png" alt="user" width="50">
+        <span class="username"></span>
+      </div>
     </div>
   </div>
 
-  <div style="background-color: blueviolet; width: 1503px;height: 900px;">
-    <div style="background-color:gray ;width: 203px;height: 900px;float :left">
-
-
+  <div class="content-container">
+    <div class="sidebar">
       <center>
-        <h2>manager Profile</h2>
+      <br><br>
+        <h2>Photographer Profile</h2><br>
       </center>
-      <form method="post" action="">
-        <center>
-          <table>
-            <tr>
-              <td width="60px"><img src="<?php echo URLROOT ?>/public/images/user.png" alt="logo" width="35"></td>
-              <td><a style=" color: #151515;text-decoration: none;font-size: 17px;" href="# info"> user info</a></td>
-            <tr>
-
-            <tr>
-              <td><img src="<?php echo URLROOT ?>/public/images/edituser.png" alt="logo" width="35"></td>
-              <td><a style=" color: #151515;text-decoration: none;font-size: 17px;" href="#edit profile"> edit
-                  profile</a></td>
-            <tr>
-
-
-            <tr>
-              <td><img src="<?php echo URLROOT ?>/public/images/settings.png" alt="logo" width="35"></td>
-              <td><a style=" color: #151515;text-decoration: none;font-size: 17px;" href="#setting"> setting</a></td>
-            <tr>
-
-          </table>
-          <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-          <table>
-            <tr>
-              <td width="60px"><img src="<?php echo URLROOT ?>/public/images/signout.png" alt="logo" width="35"></td>
-              <td><a style=" color: #151515;text-decoration: none;font-size: 17px;"
-                  href="<?php echo URLROOT ?>/PageController/logout">Logout</a></td>
-            </tr>
-          </table>
-        </center>
-        </form>
+      <nav>
+        <ul>
+          <li>
+            <a href="#photographer-info">
+              <img src="<?php echo URLROOT ?>/public/images/u.png" alt="Photographer Info" width="35">
+              Photographer Info
+            </a>
+          </li>
+          <li>
+            <a href="#upload">
+              <img src="<?php echo URLROOT ?>/public/images/upload.png" alt="Upload" width="35">
+              Upload
+            </a>
+          </li>
+          <li>
+            <a href="#edit-profile">
+              <img src="<?php echo URLROOT ?>/public/images/edituser.png" alt="Edit Profile" width="35">
+              Edit Profile
+            </a>
+          </li>
+          <li>
+            <a href="#settings">
+              <img src="<?php echo URLROOT ?>/public/images/settings.png" alt="Settings" width="35">
+              Settings
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div class="logout">
+        <a href="<?php echo URLROOT ?>/PageController/logout">
+          <img src="<?php echo URLROOT ?>/public/images/signout.png" alt="Logout" width="35">
+          Logout
+        </a>
+      </div>
     </div>
-    <div
-      style="background-image:url('../public/images/nback2.webp');background-size:cover; background-repeat: no-repeat; width: 1300px;height: 900px; float:right; ">
 
-      
+    <div class="main-content">
+      <center>
+      <br><br>
+        <h2>Details for Client</h2>
+      </center>
+      <form action="<?php echo URLROOT; ?>/clientDetailsAccept" method="post">
+        <div class="form-group">
+          <label for="photographer">Photographer Name:</label>
+          <input type="text" id="photographer" name="photographer" placeholder="Photographer Name">
+        </div>
+
+        <div class="form-group">
+          <label for="editor">Editor Information:</label>
+          <input type="text" id="editor" name="editor" placeholder="Editor Information">
+        </div>
+
+        <div class="form-group">
+  <div class="checkbox-group">
+    <input type="checkbox" id="status" name="status" value="status">
+    <label for="status">We accepted your request and already reserved a photographer and an editor</label>
+  </div>
+</div>
+
+
+        <center>
+        <br>
+          <input type="submit" value="Submit" class="submit-btn"><br><br>
+        </center>
+      </form>
+      <br><br>
+
+      <hr>
 
       <center>
-      <form action="<?php echo URLROOT; ?>/clientDetailsAccept" method="post"><br><br><br>
-        <labael style=" font-size: medium; font-weight: bold;font-size: 20px">
-          <h2>Details for client</h2></label>
-          <labale>photographer name</labale>
-          <input type="text" style="width: 50%; padding: 12px 20px; display: inline-block;  border: 1px solid #ccc; 
-   border-radius: 10px; box-sizing: border-box;margin: 8px 0; display: inline-block;" name="photographer">
-          <br>
+      <br><br>
 
-          <labale style=" font-size: medium; font-weight: bold;font-size: 20px">editor information</labale>
-          <input style="width: 50%; padding: 12px 20px; display: inline-block;  border: 1px solid #ccc; 
-   border-radius: 10px; box-sizing: border-box;margin: 8px 0; display: inline-block;" name="editor">
-          <br>
+        <h2>Manager Request for Photographer</h2>
+      </center>
+      <form action="" method="post">
+        <div class="form-group">
+          <label for="clientname">Client Name:</label>
+          <input type="text" id="clientname" name="clientname" placeholder="Client Name">
+        </div>
 
-          <input type="checkbox" id="masage" name="status" value="status">
-          <labael style=" font-size: medium;color:black;font-weight: bold;font-size: 20px" for="status">we accepted your
-            request and already reserved aphotographer and aeditor </label><br>
+        <div class="form-group">
+          <label for="eventname">Event Name:</label>
+          <input type="text" id="eventname" name="eventname" placeholder="Event Name">
+        </div>
 
-            <input type="submit" value="submit" style="width: 35%; padding: 12px 20px; display: inline-block; 
-    border: 1px solid #ccc;border-radius: 10px; box-sizing: border-box; margin: 8px 0;background-color:#8d87e8"
-              name="submit">
-            </form>
-            <br><br><br><br>
-            <hr>
-            <form action="" method="post">
-              <h2 style="font-size: 34px color: black"> manager Request for photographer</h2>
+        <div class="form-group">
+          <label for="location">Location:</label>
+          <input type="text" id="location" name="location" placeholder="Location">
+        </div>
 
-              <labal style=" font-size: medium; color: black;font-weight: bold;font-size: 20px">client name</labal>
-              <input type="text" name="clientname" placeholder="" style="width: 50%; padding: 12px 20px; display: inline-block;  border: 1px solid #ccc; 
-                          border-radius: 10px; box-sizing: border-box;margin: 8px 0; display: inline-block;"><br><br><br>
-
-              <labal style=" font-size: medium;color: black; font-weight: bold; font-size: 20px">event name </labal>
-              <input type="text" name="eventname" placeholder="" style="width: 50%; padding: 12px 20px; display: inline-block;  border: 1px solid #ccc; 
-                     border-radius: 10px; box-sizing: border-box;margin: 8px 0; display: inline-block;"><br><br><br>
-
-              <labal style=" font-size: medium; color: black; font-weight: bold;font-size: 20px">location </labal>
-              <input type="text" name="" placeholder="" style="width: 50%; padding: 12px 20px; display: inline-block;  border: 1px solid #ccc; 
-                         border-radius: 10px; box-sizing: border-box;margin: 8px 0; display: inline-block;"><br><br><br>
-
-              <input type="submit" value="submit" name="submit"style="width: 35%; padding: 12px 20px; display: inline-block; 
-                                   border: 1px solid #ccc;border-radius: 10px; box-sizing: border-box; margin: 8px 0;background-color:#8d87e8">
-
-            </form>
-
-            <center>
-
-
-              </table><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-              <table>
-                <tr>
-                  <td width="60px"><img src="<?php echo URLROOT ?>/public/images/signout.png" alt="logo" width="50">
-                  </td>
-                  <td><a style=" color: #151515;text-decoration: none;font-size: 17px;"
-                      href="<?php echo URLROOT ?>/PageController/logout">Logout</a></td>
-                </tr>
-              </table>
-            </center>
+        <center>
+          <input type="submit" value="Submit" class="submit-btn">
+        </center>
+      </form>
     </div>
-    <div style="background-color:  aliceblue; width: 1000px;height: 780px; float:right; ">
-
-
-      </div">
-
-    </div>
+  </div>
 </body>
 
 </html>
